b7d534182ac92fb50018fe9f843bd8d67217b16d
unknown <olef.cluzef@gmail.com>
2026-05-03 01:24:42 +0500

    Chat_bot

--- Telegrambot_chat/config/config.py
+++ Telegrambot_chat/config/config.py
@@ -1,10 +1,11 @@
 # Токены и подключения
-BOT_TOKEN = '...'
-DB_URL = "..."
+# Ollama
+BOT_TOKEN = "8519258791:AAH5qhzfEtFyRFNVv70ir39XJJ9DR81_kOI"
+DB_URL = "postgresql://postgres:Tinkoff2307@192.168.0.115:5432/TelegramBot"
 
 # Ollama
 OLLAMA_MODEL = "qwen3:4b"  # или ваша модель
-OLLAMA_URL = "..."
+OLLAMA_URL = "http://192.168.0.115:11434/api/generate"
 
 # Прочее
 DEFAULT_SYSTEM_PROMPT = (
